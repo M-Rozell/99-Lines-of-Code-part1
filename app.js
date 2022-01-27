a833d786a96300a463cf28632d34b6c1ff6163b3
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-01-27 09:52:46 -0600

    Added colon after each name.

--- app.js
+++ app.js
@@ -3,33 +3,33 @@ var friends = ['Cat', 'James', 'Tommy', 'Rachel', 'Sonya'];
 
 
 
-for (let i = 0; i<friends.length; i++)
-{
-let friendsEach=friends[i];
-console.log(friendsEach);
+for (let i = 0; i < friends.length; i++) {
+    let friendsEach = friends[i];
+    console.log(friendsEach + ":");
 
-for(let t=99; t>0; t--) {
-var word = "lines"
-if(t===1){ var word ='line'
-};
-  
-    
-  if(t>2){
-        console.log(t + " " + word + " of code in the file,",  t,  word,  "of code;", 
-        friendsEach,  "strikes one out, cleares it all out,",  t-1, 
-        "lines of code in the file.");
+    for (let t = 99; t > 0; t--) {
+        var word = "lines"
+        if (t === 1) {
+            var word = 'line'
+        };
+
+
+        if (t > 2) {
+            console.log(t + " " + word + " of code in the file,", t, word, "of code;",
+                friendsEach, "strikes one out, cleares it all out,", t - 1,
+                "lines of code in the file.");
+        };
+
+        if (t == 2) {
+            console.log(t + " " + word + ' of code in the file,', t, word, 'of code;',
+                friendsEach, 'strikes one out, cleares it all out,', t - 1, 'line of code in the file.');
+        };
+
+        if (t == 1) {
+            console.log(t + " " + word + ' of code in the file,', t, word, 'of code;',
+                friendsEach, 'strikes one out, cleares it all out, no more lines of code in the file.');
+        }
+
     };
-
-     if(t==2){
-        console.log(t + " " + word + ' of code in the file,', t, word, 'of code;',
-        friendsEach, 'strikes one out, cleares it all out,', t-1, 'line of code in the file.');
-    };
-
-    if(t==1){
-        console.log(t + " " + word + ' of code in the file,', t, word, 'of code;',
-        friendsEach, 'strikes one out, cleares it all out, no more lines of code in the file.');
-    }
-
-};    
 
 };
